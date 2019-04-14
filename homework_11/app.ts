b0d37bfc64bc9140789fc4e75d439a59fd98e378
Vorleak Chy <vorleak.chy@gmail.com>
2019-04-14 18:38:37 -0500

    Add homework 11

---
+++ homework_11/app.ts
@@ -0,0 +1,10 @@
+class University {
+    constructor(public name: string, public dept: string) {}
+
+    graduation(year: number): void {
+        console.log(`Graduating ${this.dept} ${year} students`);;
+    }
+}
+
+const mum = new University('MUM', 'Computer Science');
+mum.graduation(2019);
